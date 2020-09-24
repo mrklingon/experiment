23ec798870af6ea34b5314f6923600437409b49c
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2020-09-24 15:50:55 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.showIcon(IconNames.Heart)
+basic.pause(1000)
+basic.showString("Hello!")
+basic.showLeds(`
+    . . . . .
+    # # # . #
+    . . # . #
+    . . . # #
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
